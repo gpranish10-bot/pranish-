edd3444c79f3b426efef2276c5f6bf63642c7db3
Pranish Gautam <p.gautam@rgu.ac.uk>
2025-09-19 11:08:41 +0100

    add ary.js

---
+++ ary.js
@@ -0,0 +1,6 @@
+let myArray = [[5,4,3,2,1],
+               [6,7,8,9,10],
+               [11,12,13,14,15]];
+ console.log(myArray.lenght);   
+ console.log(myArray[0].lenght);                       
+
